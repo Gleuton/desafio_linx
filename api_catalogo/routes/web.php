49308288f557b9c08906aab39b37a1c20bc2be88
Gleuton <gleuton.dutra@gmail.com>
2021-09-16 17:01:29 -0300

    configurar swagger para documentar a aplicação

--- api_catalogo/routes/web.php
+++ api_catalogo/routes/web.php
@@ -1,11 +1,57 @@
 <?php
 
 /** @var \Laravel\Lumen\Routing\Router $router */
-
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+/**
+ * @OA\Info(title="API de Catalogo", version="1.0")
+ */
+$router->get('/', function () {
+    return view('swagger');
 });
 
-$router->get('/{id}/compact', 'ProductController@compact');
-$router->get('/{id}/complete', 'ProductController@complete');
+$router->get('/api/docJson', function () {
+    $openapi = \OpenApi\Generator::scan([__DIR__]);
+    header('Content-Type: application/json');
+    return $openapi->toJson();
+});
+/**
+ * @OA\Get(
+ *     path="/api/{id}/compact",
+ *     tags={"Produtos"},
+ *     @OA\Parameter(
+ *          name="id",
+ *          in="path",
+ *          required= true,
+ *          description="id do produto",
+ *     ),
+ *     @OA\Response(
+ *          response="200",
+ *          description="retorna os dados basicos produto referente ao id"
+ *     )
+ * )
+ */
+$router->get(
+    '/api/{id}/compact',
+    'ProductController@compact'
+);
+
+/**
+ * @OA\Get(
+ *     path="/api/{id}/complete",
+ *     tags={"Produtos"},
+ *     @OA\Parameter(
+ *          name="id",
+ *          in="path",
+ *          required= true,
+ *          description="id do produto",
+ *     ),
+ *     @OA\Response(
+ *          response="200",
+ *          description="retorna os dados completos produto referente ao id"
+ *     )
+ * )
+ */
+$router->get(
+    '/api/{id}/complete',
+    'ProductController@complete'
+);
 
